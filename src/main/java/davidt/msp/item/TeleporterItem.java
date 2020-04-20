2363d80fa9ce6d0067801ffd40f862739cf7cbf0
David Triphon <david.triphon@gmail.com>
2020-04-20 15:50:25 -0700

    removed nether portal from switching dimensions.

--- src/main/java/davidt/msp/item/TeleporterItem.java
+++ src/main/java/davidt/msp/item/TeleporterItem.java
@@ -1,13 +1,18 @@
 package davidt.msp.item;
 
 import davidt.msp.dimension.mod.*;
+import net.minecraft.entity.*;
 import net.minecraft.entity.player.*;
 import net.minecraft.item.*;
 import net.minecraft.util.*;
 import net.minecraft.world.*;
 import net.minecraft.world.dimension.*;
+import net.minecraft.world.server.*;
 import net.minecraftforge.common.*;
+import net.minecraftforge.common.util.*;
 import org.apache.logging.log4j.*;
+
+import java.util.function.*;
 
 import static davidt.msp.MinersSpaceProgram.*;
 
@@ -18,7 +23,18 @@ public class TeleporterItem extends Item
      new ResourceLocation(MOD_ID, "msp_space_dim_name_1");
    
    private static final Item.Properties DEFAULT_PROPERTIES = new Item.Properties();
+   
    private static final Logger LOGGER = LogManager.getLogger();
+   
+   private static final ITeleporter PORTALLESS = new ITeleporter()
+   {
+      @Override
+      public Entity placeEntity(Entity entity, ServerWorld currentWorld, ServerWorld destWorld,
+        float yaw, Function <Boolean, Entity> repositionEntity)
+      {
+         return repositionEntity.apply(false);
+      }
+   };
    
    
    public TeleporterItem()
@@ -46,7 +62,7 @@ public class TeleporterItem extends Item
          
          LOGGER.debug("teleported {} to {}", playerIn, destination);
          
-         playerIn.changeDimension(destination);
+         playerIn.changeDimension(destination, PORTALLESS);
       }
       
       return ActionResult.resultSuccess(playerIn.getHeldItem(handIn));
